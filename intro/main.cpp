4472911f615bfbbb89efedbfcea37306907c2e0e
David Saah <dasorange.hope@gmail.com>
2024-01-15 15:44:28 +0000

    add hello world

---
+++ intro/main.cpp
@@ -0,0 +1,16 @@
+/* #include <iostream> */
+/**/
+/* int main (int argc, char *argv[]) { */
+/*   std::cout << "Hello world" << std::endl; */
+/*   std::cout << "Hello world" << std::endl; */
+/*   std::cout << "Hello world" << std::endl; */
+/*   std::cout << "Hello world" << std::endl; */
+/*   return 0; */
+/* } */
+
+#include <stdio.h>
+
+int main() {
+  printf("Hello World\n");
+  return 0;
+}
