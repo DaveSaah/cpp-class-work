7eaafbe67a67c7b99b6587429aafe8daa74ba46b
David Saah <dasorange.hope@gmail.com>
2024-01-16 06:29:35 +0000

    add sqrt example

---
+++ sqrt/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <math.h>
+
+int main() {
+  float x;
+  std::cout << "Enter a real number: ";
+  std::cin >> x;
+
+  std::cout << "The square root of " << x << " is: " << sqrt(x) << std::endl;
+}
