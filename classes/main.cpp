5d38cee70a88ed49197421aacce4a4b90dd3f840
David Saah <dasorange.hope@gmail.com>
2024-01-17 15:43:36 +0000

    add object pointer example

--- classes/main.cpp
+++ classes/main.cpp
@@ -17,8 +17,19 @@ class IntCell {
 };
 
 int main () {
-  IntCell cell;
-  std::cout << cell.read() << std::endl;
+  IntCell m1;
+  IntCell m2(12);
+  IntCell *m3;
+
+  m1.write(44);
+  m2.write(m1.read() + 1);
+
+  std::cout << "m1: " << m1.read() << std::endl;
+  std::cout << "m2: " << m2.read() << std::endl;
+
+  m3 = new IntCell;
+
+  std::cout << "m3: " << m3->read() << std::endl;
   return 0;
 }
 
